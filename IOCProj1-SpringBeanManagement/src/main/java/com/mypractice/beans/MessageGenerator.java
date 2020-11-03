3644cd2d5f6f01d83ecf020b71a2fe9389ccfddf
Nasruddinkhan <nasruddinkhan44@gmail.com>
2020-11-03 16:42:41 +0530

    first ioc project

---
+++ IOCProj1-SpringBeanManagement/src/main/java/com/mypractice/beans/MessageGenerator.java
@@ -0,0 +1,15 @@
+package com.mypractice.beans;
+
+public class MessageGenerator {
+
+	
+	public MessageGenerator() {
+		super();
+		// TODO Auto-generated constructor stub
+		System.out.println("MessageGenerator.MessageGenerator()");
+	}
+	public String sayHello(String user) {
+		return "Hi "+user;
+	}
+	
+}
