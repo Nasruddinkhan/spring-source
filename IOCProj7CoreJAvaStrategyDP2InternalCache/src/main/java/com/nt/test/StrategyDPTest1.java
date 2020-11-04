8b0439af0c65fa7322997abd6238af6fa103e56b
Nasruddinkhan <nasruddinkhan44@gmail.com>
2020-11-04 17:38:17 +0530

    strategy design patter

---
+++ IOCProj7CoreJAvaStrategyDP2InternalCache/src/main/java/com/nt/test/StrategyDPTest1.java
@@ -0,0 +1,21 @@
+package com.nt.test;
+
+import com.mypractice.beans.Viechle;
+import com.nt.factory.ViechleFactory;
+
+public class StrategyDPTest1{
+
+	public static void main(String[] args) {
+		Viechle viechle=null;
+		//Get Target class obj having Dependent class obj
+		viechle=ViechleFactory.getInstance("CNG");
+		viechle.journey("MUMBAI", "CHENNAI");
+		System.out.println("....................................");
+		//Get Target class obj having Dependent class obj
+				viechle=ViechleFactory.getInstance("petrol");
+				viechle.journey("CHENNAI", "MUMBAI");
+				viechle=ViechleFactory.getInstance("petrol");
+				viechle.journey("DELHI", "MUMBAI");
+	}
+
+}
