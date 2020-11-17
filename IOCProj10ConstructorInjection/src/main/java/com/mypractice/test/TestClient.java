5d7681567c98a50a1e4233946cac3c9463f9334b
Nasruddinkhan <nasruddinkhan44@gmail.com>
2020-11-17 18:58:58 +0530

    constructor injection with params

--- IOCProj10ConstructorInjection/src/main/java/com/mypractice/test/TestClient.java
+++ IOCProj10ConstructorInjection/src/main/java/com/mypractice/test/TestClient.java
@@ -6,6 +6,7 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
 import com.mypractice.bean.Employee;
+import com.mypractice.bean.SalaryCalulation;
 
 public class TestClient {
 	public static void main(String[] args) {
@@ -15,5 +16,7 @@ public class TestClient {
 		bean = new XmlBeanFactory(res);
 		Employee emp = bean.getBean("emp", Employee.class);
 		System.out.println(emp);
+		SalaryCalulation calulation = bean.getBean("salary", SalaryCalulation.class);
+		System.out.println(calulation);
 	}
 }
