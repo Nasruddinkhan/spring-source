1b06685fc65e3d8500d2372d89157c1f4918354a
Nasruddinkhan <nasruddinkhan44@gmail.com>
2020-11-17 19:07:36 +0530

    create cyclic dependency

---
+++ IOCProj11CyclicDIUsingSetterAndConstructor/src/main/java/com/mypracice/bean/A.java
@@ -0,0 +1,17 @@
+package com.mypracice.bean;
+
+public class A {
+	private B b;
+
+	public A(B b) {
+		super();
+		this.b = b;
+	}
+
+	@Override
+	public String toString() {
+		return "A [b=" + b + "]";
+	}
+	
+	
+}
---
+++ IOCProj11CyclicDIUsingSetterAndConstructor/src/main/java/com/mypracice/bean/B.java
@@ -0,0 +1,16 @@
+package com.mypracice.bean;
+
+public class B {
+	private A a;
+
+	public B(A a) {
+		super();
+		this.a = a;
+	}
+
+	@Override
+	public String toString() {
+		return "B [a=" + a + "]";
+	}
+
+}
