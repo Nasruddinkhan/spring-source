3b305b9517eb25ff120ba18eb0357c0c4b11da6c
Nasruddinkhan <nasruddinkhan44@gmail.com>
2020-12-05 22:53:49 +0530

    RealTimeDI, LayeredApp and InnerBeansSupport

--- IOCProj13-InnerBeans/src/main/java/com/mypractice/test/Test.java
+++ IOCProj13-InnerBeans/src/main/java/com/mypractice/test/Test.java
@@ -1,9 +1,14 @@
 package com.mypractice.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.mypractice.bean.Result;
+import com.mypractice.main.MainController;
+import com.mypractice.vo.EmployeeVOBuilder;
 
 public class Test {
 
@@ -13,6 +18,14 @@ public class Test {
 		System.out.println(context);
 		Result result = context.getBean("result", Result.class);
 		System.out.println(result.result());
-		
+		MainController main = context.getBean("main", MainController.class);
+		List<Double> compensition = new ArrayList<>();
+		compensition.add(8000.0);
+		compensition.add(6000.0);
+		compensition.add(7000.0);
+		compensition.add(9000.0);
+		System.out.println(main.operation(
+				new EmployeeVOBuilder().setEmpID("101").setName("Nasruddin").setCompensition(compensition).build()));
+
 	}
 }
